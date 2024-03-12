fbe93490792efeee2a04685ba51f77b0a9e76b6d
pfrito <carlos.rolando.morell@gmail.com>
2024-03-12 17:26:31 -0400

    fetching categories from server/api

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
+<script setup>
+let visible = ref(false);
+const { data: categories } = await useFetch("/api/constants");
+</script>
+
 <template>
   <body class="bg-gray-100 text-gray-600 font-nunito">
     <main>
@@ -79,6 +84,39 @@
             </div>
 
             <label for="types">Type of expense</label>
+            <div class="block mt-1 mb-3 text-gray-800 relative">
+              <input
+                type="text"
+                name="types"
+                id="types"
+                class="relative w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-gray-800 peer"
+                @focus="visible = true"
+                @blur="visible = false"
+              />
+
+              <div
+                v-if="visible"
+                class="bg-white w-full mt-1 px-3 py-2 rounded-lg border border-gray-300 absolute"
+              >
+                <div v-for="category in categories" class="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="{1.5}"
+                    stroke="currentColor"
+                    class="w-6 pr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      :d="category.path"
+                      />
+                  </svg>
+                  <span>{{ category.name }}</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
