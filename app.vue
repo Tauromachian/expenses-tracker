65e3ae21d2f2c00b2aed838110c4a1e5b0868fc2
pfrito <carlos.rolando.morell@gmail.com>
2024-03-17 10:25:03 -0400

    put footer code into a component

--- app.vue
+++ app.vue
@@ -245,33 +245,6 @@ function contactMe(url) {
         </Form>
       </div>
     </main>
-    <footer class="flex flex-col items-center font-mono text-gray-600 pb-5">
-      <hr class="h-2 w-full max-w-md pb-5" />
-      <div class="flex gap-2 mb-2">
-        <Icon
-          name="uil:whatsapp"
-          class="w-7 h-7 cursor-pointer hover:text-gray-800 transform hover:scale-125 transition ease-in-out duration-200"
-          @click="contactMe(`https://wa.me/5356653917`)"
-        />
-        <Icon
-          name="uil:telegram"
-          class="w-7 h-7 cursor-pointer hover:text-gray-800 transform hover:scale-125 transition ease-in-out duration-200"
-          @click="contactMe(`https://t.me/cmorellr`)"
-        />
-        <Icon
-          name="uil:github"
-          class="w-7 h-7 cursor-pointer hover:text-gray-800 transform hover:scale-125 transition ease-in-out duration-200"
-          @click="contactMe(`https://github.com/pfrito`)"
-        />
-      </div>
-      <div class="flex flex-col mobile:flex-row items-center">
-        <span>©2024 Designed, created and maintained by </span>
-        <a
-          href="https://github.com/pfrito"
-          class="uppercase hover:text-gray-800 mobile:ml-2"
-          >Carlos</a
-        >
-      </div>
-    </footer>
+    <Footer @contactMe="contactMe"></Footer>
   </div>
 </template>
