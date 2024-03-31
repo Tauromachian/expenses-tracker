2ab88587653c92c7d7edb539634ed8d7ea2a7bc7
pfrito <carlos.rolando.morell@gmail.com>
2024-03-30 20:04:05 -0400

    dropwdown select for types in stats card

--- components/ExpenseStatsCard.vue
+++ components/ExpenseStatsCard.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { initFlowbite } from 'flowbite'
 import { useExpenseStore } from "../stores/expenses";
 
 const props = defineProps({
@@ -10,9 +9,10 @@ const expenseStore = useExpenseStore();
 let type = ref("All");
 let expenses = ref([]);
 let chartKey = ref(0);
+let visibleTypes = ref(false);
+let fadingInOutTypes = ref(false);
 
 onMounted(() => {
-  initFlowbite()
   loadData();
 });
 
@@ -27,9 +27,18 @@ function loadData() {
   chartKey.value++;
 }
 
+function handleEditType() {
+  visibleTypes.value = true;
+  setTimeout(() => {
+    fadingInOutTypes.value = true;
+  }, 10);
+}
+
 function changeType(data) {
+  if (data != type.value) loadData();
   type.value = data;
-  loadData();
+  fadingInOutTypes.value = false;
+  visibleTypes.value = false;
 }
 
 function getCategory(category) {
@@ -47,7 +56,7 @@ function removeExpense(id) {
 </script>
 
 <template>
-  <div class="bg-white mobile:rounded-md px-7 pt-4 pb-3">
+  <div class="bg-white mobile:rounded-md px-7 pt-4 pb-3" @click="visibleTypes = false">
     <DonutChart
       :key="chartKey"
       v-if="expenses.length"
@@ -56,32 +65,30 @@ function removeExpense(id) {
     ></DonutChart>
 
     <div class="flex justify-between mb-2">
-      <div class="text-gray-800 font-bold flex items-center">
+      <div class="text-gray-800 font-bold flex items-center relative">
         <span class="text-lg">{{ type }} expenses</span>
         <div class="text-gray-400 border-l-2 ml-3">
-          <button
-            type="button"
-            id="dropdownButton"
-            data-dropdown-toggle="dropdown"
-          >
+          <button type="button" @click.stop="handleEditType">
             <Icon
               name="fluent:edit-28-filled"
               class="ml-1 w-7 h-7 hover:text-gray-800 hover:bg-gray-100 p-1 rounded-full transition cursor-pointer duration-100 ease-in-out"
             ></Icon>
           </button>
         </div>
-        <div id="dropdown" class="z-10 hidden">
+        <div
+          v-if="visibleTypes"
+          class="font-normal bg-white w-32 mt-1 p-2 rounded-lg border border-gray-300 absolute top-0 right-7 z-10 transition-opacity duration-150 ease-in-out"
+          :class="{
+            'opacity-100': fadingInOutTypes,
+            'opacity-0': !fadingInOutTypes,
+          }"
+        >
           <div
-            aria-labelledby="dropdownDefaultButton"
-            class="font-normal bg-white w-32 mt-1 p-2 rounded-lg border border-gray-300"
+            v-for="choice in ['All', 'One time', 'Monthly', 'Anual']"
+            class="pl-2 flex h-7 items-center cursor-pointer hover:bg-gray-200 rounded-full"
+            @click="changeType(choice)"
           >
-            <div
-              v-for="choice in ['All', 'One time', 'Monthly', 'Anual']"
-              class="pl-2 flex h-7 items-center cursor-pointer hover:bg-gray-200 rounded-full"
-              @click="changeType(choice)"
-            >
-              <span>{{ choice }}</span>
-            </div>
+            <span>{{ choice }}</span>
           </div>
         </div>
       </div>
