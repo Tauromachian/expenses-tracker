370a9138fa74521210caf702f70f5cb442e6361b
pfrito <carlos.rolando.morell@gmail.com>
2024-03-31 13:19:49 -0400

    fix root: wrong settings to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/Expenses-Tracking/",
+    buildAssetsDir: "assets",
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
@@ -8,9 +12,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: [
-    "@vee-validate/nuxt",
-    "nuxt-icon",
-    "@pinia/nuxt",
-  ],
+  modules: ["@vee-validate/nuxt", "nuxt-icon", "@pinia/nuxt"],
 });
