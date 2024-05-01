ab915770e726b70e03926dec1c8cd2e82915bb81
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-04-30 20:59:30 -0400

    Remove unnecessary options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: "/Expenses-Tracking/",
-    buildAssetsDir: "assets",
-  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
