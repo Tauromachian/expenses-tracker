24044e180baba19a252a60e2963567c45c7a1675
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-05-02 16:49:29 -0400

    Make BaseButton component

---
+++ components/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="bg-teal-700 text-white px-3 py-2 text-teal-700 rounded-full focus:bg-teal-700 focus:text-white hover:bg-teal-700 hover:text-white transition"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+defineProps({});
+</script>
+
+<style lang="scss" scoped></style>
