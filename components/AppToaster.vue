c061226a15a9d854e40343cc3e2884eb14449247
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-05-07 16:31:23 -0400

    Make basic AppToaster component

---
+++ components/AppToaster.vue
@@ -0,0 +1,41 @@
+<script setup>
+const messages = ref([]);
+const showToast = ref(false);
+
+const props = defineProps({
+  message: {
+    type: String,
+    default: "",
+  },
+});
+
+watch(props.message, (newValue) => {
+  if (!newValue) return;
+
+  openToast();
+});
+
+const openToast = (message) => {
+  messages.value.push(message);
+  showToast.value = true;
+  closeToast();
+};
+
+const closeToast = () => {
+  setTimeout(() => {
+    messages.value.shift();
+  }, 160);
+};
+
+defineExpose({ openToast });
+</script>
+
+<template>
+  <div class="flex items-center w-full max-w-xs p-4 mb-4 right-4 top-2 fixed">
+    <AppToast
+      v-for="(oneMessage, index) in messages"
+      :key="index + oneMessage"
+      :message="oneMessage"
+    ></AppToast>
+  </div>
+</template>
