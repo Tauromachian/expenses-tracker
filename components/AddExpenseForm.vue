be1d8acfe5669e51b828603d99961e3a0de1eac4
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-07 21:55:28 -0500

    Simplify icon

--- components/AddExpenseForm.vue
+++ components/AddExpenseForm.vue
@@ -83,29 +83,11 @@ function handleFocusCategory() {
 
 <template>
   <AppCard>
-    <div class="bg-teal-50 flex mobile:rounded-t-md">
+    <div class="bg-teal-50 flex gap-4 mobile:rounded-t-md py-5 px-7">
       <Icon
-        icon="material-symbols:close"
-        class="w-10 h-10 text-teal-700 rounded-xl my-5 mx-7 shadow-md p-2"
-        width="24px"
-        height="24px"
+        name="material-symbols:chart-data-outline-rounded"
+        class="w-8 h-8 text-teal-700 rotate-90"
       />
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="{1.5}"
-          stroke="currentColor"
-          class="w-10 text-teal-700 rounded-xl my-5 mx-7 shadow-md p-2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
-          />
-        </svg>
-      </div>
       <div class="my-auto">
         <span class="font-bold block text-lg">Add new expense</span>
         <span class="text-xs">
