c84a7d6beaf39f4f35e85d1785b626ae1f6e69e0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-08 20:33:02 -0500

    Reduce size of error text

--- components/ErrorText.vue
+++ components/ErrorText.vue
@@ -2,7 +2,7 @@
 
 <template>
   <span
-    class="text-red-500 block absolute empty:opacity-0 opacity-100 transition"
+    class="text-red-500 text-sm block absolute empty:opacity-0 opacity-100 transition"
   >
     <slot></slot>
   </span>
