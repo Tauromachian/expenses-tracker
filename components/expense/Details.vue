1a6d609085c70487294be78af32d1428e9590ad5
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-12 20:18:15 -0400

    Remove unnecessary tooltip

--- components/expense/Details.vue
+++ components/expense/Details.vue
@@ -35,15 +35,6 @@ function handleRemove() {
       />
     </span>
 
-    <div
-      :id="`tooltip-${details.id}`"
-      role="tooltip"
-      class="absolute z-10 invisible inline-block px-3 py-2 text-sm text-white bg-gray-700 rounded-lg shadow-sm opacity-0 tooltip transition-opacity duration-300 ease-in-out"
-    >
-      {{ category.name }}
-      <div class="tooltip-arrow" data-popper-arrow></div>
-    </div>
-
     <div class="w-full flex">
       <div class="flex flex-col text-gray-800">
         <span class="font-bold">{{ details.name }}</span>
