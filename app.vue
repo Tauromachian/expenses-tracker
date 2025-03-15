bf8931fabd7378fdf5e7cd3996a73b25a4b27a1e
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-15 09:26:53 -0400

    Add margin on small screens

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main class="font-nunito min-h-screen bg-gray-100">
-    <div class="max-w-7xl mx-auto pt-10">
+    <div class="max-w-7xl mx-4 lg:mx-auto pt-10">
       <NavBar class="mb-8"></NavBar>
 
       <NuxtPage />
