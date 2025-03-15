68d8422e20e28aab856d0f03ddb9d10a112923b8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-15 09:43:31 -0400

    Add inputClass prop

--- components/AppInput.vue
+++ components/AppInput.vue
@@ -2,6 +2,7 @@
 const props = defineProps({
   label: { type: String, default: "" },
   error: { type: String, default: "" },
+  inputClass: { type: String, default: "" },
 });
 </script>
 
@@ -10,7 +11,7 @@ const props = defineProps({
     <label :for="$attrs.name">{{ label }}</label>
     <div class="mt-1 mb-5 text-gray-800 relative">
       <slot name="prepend"></slot>
-      <Field v-bind="$attrs" class="text-field peer" />
+      <Field v-bind="$attrs" class="text-field peer" :class="inputClass" />
       <ErrorText> {{ error }}</ErrorText>
     </div>
   </div>
