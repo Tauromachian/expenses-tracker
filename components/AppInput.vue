ece33eaaaa30418d92bf6bd7b961ab2d6d3b4b89
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-15 09:03:42 -0400

    Make basic AppInput component

---
+++ components/AppInput.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  label: { type: String, default: "" },
+  error: { type: String, default: "" },
+});
+</script>
+
+<template>
+  <div>
+    <label :for="$attrs.name">{{ label }}</label>
+    <div class="mt-1 mb-5 text-gray-800 relative">
+      <slot name="prepend"></slot>
+      <Field v-bind="$attrs" class="text-field peer" />
+      <ErrorText> {{ error }}</ErrorText>
+    </div>
+  </div>
+</template>
