b53fa3a6e5a55495ff408b4e7f8ea4f1223bbf6c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-16 15:01:39 -0400

    Add missing logic

--- pages/compound-calculator.vue
+++ pages/compound-calculator.vue
@@ -1,16 +1,34 @@
-<script setup></script>
+<script setup>
+const fadingInOutArrow = ref(false);
+const appToaster = ref();
+
+function submitForm(form) {
+  expenseStore.addExpense(form);
+
+  if (!appToaster?.value) return;
+
+  appToaster.value.openToast("New expense added!");
+}
+
+function scrollToStats() {
+  const component = document.getElementById("expense-stats");
+
+  if (!component) return;
+
+  component.scrollIntoView({ behavior: "smooth" });
+
+  closeScrollDown();
+}
+</script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold mb-5">Compound Calculator</h1>
 
     <div class="flex flex-col md:flex-row gap-5 md:items-start">
-      <CompoundForm
-        :categories="categories"
-        @submit-form="submitForm"
-      ></CompoundForm>
+      <CompoundForm @submit="submitForm"></CompoundForm>
 
-      <ExpenseStatsCard :categories="categories"> </ExpenseStatsCard>
+      <CompoundStatsCard> </CompoundStatsCard>
     </div>
 
     <AppToaster ref="appToaster"></AppToaster>
