408b98467ffa7b005a4349d42fef02691e5ef381
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-16 14:52:54 -0400

    Improve error message

--- utils/rules.ts
+++ utils/rules.ts
@@ -8,5 +8,5 @@ export function positiveNumber(value: string) {
   if (!value) return "This field is required";
 
   if (/^[0-9]+$/.test(value) && parseFloat(value) > 0) return true;
-  return "Invalid amount";
+  return "This field needs to be positive number";
 }
