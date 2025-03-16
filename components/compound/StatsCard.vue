9b80bc284b1b1c3f61c2dee34b92696b718a9b62
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-16 15:03:58 -0400

    Remove unnecessary code

--- components/compound/StatsCard.vue
+++ components/compound/StatsCard.vue
@@ -1,45 +1,8 @@
 <script setup>
-import { useExpenseStore } from "../stores/expenses";
-
-const props = defineProps({
-  categories: {
-    type: Array,
-    required: true,
-  },
-});
-const expenseStore = useExpenseStore();
-
-const type = ref("Annual");
 const chartKey = ref(0);
-const expenseTypes = ref(["Monthly", "Annual"]);
-const selectedExpenseType = ref("Annual");
 
-const expenses = computed(() => {
-  const allExpenses = expenseStore.expenses;
-  const expenses = [
-    ...allExpenses.filter((expense) => {
-      if (type.value == "Annual") return true;
-      return expense.types == type.value;
-    }),
-  ];
-
-  return expenses;
-});
-
-watch(expenses, () => {
-  chartKey.value++;
-});
-
-function getCategory(category) {
-  return props.categories[
-    props.categories.findIndex((item) => {
-      return item.name == category;
-    })
-  ];
-}
-
-function removeExpense(id) {
-  expenseStore.removeExpense(id);
+function removeInvestment(id) {
+  expenseStore.removeInvestment(id);
 }
 </script>
 
@@ -47,26 +10,25 @@ function removeExpense(id) {
   <div id="expense-stats" class="w-full max-w-md flex mobile:items-center">
     <AppCard>
       <AppCardBody>
-        <ExpenseDonutChart
+        <InvestmentLineChart
           v-if="expenses?.length"
           :key="chartKey"
           :expenses="expenses"
-          :categories="props.categories"
-        ></ExpenseDonutChart>
+        ></InvestmentLineChart>
 
         <div class="max-h-screen overflow-scroll relative">
           <div v-if="!expenses?.length" class="text-center my-20">
             No investments! Add one
           </div>
-          <ExpenseDetails
+          <InvestmentItem
             v-for="expense in expenses"
             v-else
             :key="expense.id"
             :details="expense"
             :category="getCategory(expense.categories)"
             class="border-b last:border-0"
-            @remove-expense="removeExpense(expense.id)"
-          ></ExpenseDetails>
+            @remove-expense="removeInvestment(expense.id)"
+          ></InvestmentItem>
         </div>
       </AppCardBody>
     </AppCard>
